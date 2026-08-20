41d14cde90b258ff11308b96d5f1e73278b70b47
Kishore-Dev <kishoreprakash45@gmail.com>
2026-08-20 23:17:43 +0530

    Reformat import statements in App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 
 import './App.css';
 
-import Navbar from './components/Layouts/Navbar';
+
 import Footer from './components/Layouts/Footer';
 import Main from './components/Main';
 import WebFont from 'webfontloader';
